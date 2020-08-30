a6ce606d55e59c93d0cc67e3900819764ea22353
Mayur Rathod <rathodmayur93@gmail.com>
2020-08-30 18:24:29 +0300

    Initial Commit

---
+++ InternetConnection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InternetConnection
+//
+//  Created by ds-mayur on 30/8/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
